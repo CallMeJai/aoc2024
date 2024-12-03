823637689328b8dd3c77f180f2b73e4daa66739a
Jaiveer <32986920+CallMeJai@users.noreply.github.com>
2024-12-02 23:10:16 -0800

    day 02 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let input = include_str!("../rsrc/input.txt");
     let result = part_1(input);
     println!("part 1: {result}");
+    let result = part_2(input);
+    println!("part 2: {result}");
 }
 
 enum Direction {
@@ -10,34 +12,58 @@ enum Direction {
     Unset
 }
 
-fn part_1(input: &str) -> usize {
-    let mut safe_count = 0;
-    'line: for line in input.lines() {
-        let mut direction = Direction::Unset;
-        let mut splits = line.split(' ');
-        let mut current = splits.next()
+fn is_simply_safe(splits: &mut dyn Iterator<Item = &str>) -> bool {
+    let mut direction = Direction::Unset;
+    let mut current = splits.next()
             .expect("split doesn't have a first element").parse::<i64>()
             .expect("couldn't parse value");
-        for split in splits {
-            let next = split.parse::<i64>().expect("couldn't parse value");
-            match current - next {
-                ..-3 => continue 'line,
-                -3..0 => match direction {
-                            Direction::Unset => direction = Direction::Decreasing,
-                            Direction::Increasing => continue 'line,
-                            Direction::Decreasing => (),
-                        },
-                0 => continue 'line,
-                1..4 => match direction {
-                            Direction::Unset => direction = Direction::Increasing,
-                            Direction::Increasing => (),
-                            Direction::Decreasing => continue 'line,
-                        },
-                4.. => continue 'line,
-            }
-            current = next;
+    for split in splits {
+        let next = split.parse::<i64>().expect("couldn't parse value");
+        match current - next {
+            ..-3 => return false,
+            -3..0 => match direction {
+                        Direction::Unset => direction = Direction::Decreasing,
+                        Direction::Increasing => return false,
+                        Direction::Decreasing => (),
+                    },
+            0 => return false,
+            1..4 => match direction {
+                        Direction::Unset => direction = Direction::Increasing,
+                        Direction::Increasing => (),
+                        Direction::Decreasing => return false,
+                    },
+            4.. => return false,
         }
-        safe_count += 1;
+        current = next;
+    }
+    true
+}
+
+fn part_1(input: &str) -> usize {
+    let mut safe_count = 0;
+    for line in input.lines() {
+        let mut splits = line.split(' ');
+        if is_simply_safe(&mut splits) {
+            safe_count += 1;
+        }
+    }
+    safe_count
+}
+
+fn part_2(input: &str) -> usize {
+    let mut safe_count = 0;
+    'line: for line in input.lines() {
+        let splits = line.split(' ');
+        if is_simply_safe(&mut splits.clone()) {
+            safe_count += 1;
+            continue 'line;
+        }
+        for i in 0..splits.clone().count() {
+            if is_simply_safe(&mut splits.clone().take(i).chain(splits.clone().skip(i + 1))) {
+                safe_count += 1;
+                continue 'line;
+            }
+        }
     }
     safe_count
 }
@@ -49,5 +75,11 @@ mod tests {
     fn day_02_part_1_test() {
         let input = include_str!("../rsrc/test.txt");
         assert_eq!(part_1(input), 2);
+    }
+
+    #[test]
+    fn day_02_part_2_test() {
+        let input = include_str!("../rsrc/test.txt");
+        assert_eq!(part_2(input), 4);
     }
 }
